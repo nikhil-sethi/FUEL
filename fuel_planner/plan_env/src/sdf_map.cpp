0ebd1b00be76eac8861bf5bd1c2b969a9766281c
nikhil-sethi <sethi.nirvil@gmail.com>
2024-04-06 15:48:48 +0200

    [*] Lowered frontier z axis limit
    - tested in hw and sim
    -this is experimental, it has been tested but not that well

--- fuel_planner/plan_env/src/sdf_map.cpp
+++ fuel_planner/plan_env/src/sdf_map.cpp
@@ -300,13 +300,13 @@ void SDFMap::inputPointCloud(
       length = (pt_w - camera_pos).norm();
       if (length > mp_->max_ray_length_)
         pt_w = (pt_w - camera_pos) / length * mp_->max_ray_length_ + camera_pos;
-      if (pt_w[2] < 0.2) continue;
+      if (pt_w[2] < 0.1) continue;
       tmp_flag = 0;
     } else {
       length = (pt_w - camera_pos).norm();
       if (length > mp_->max_ray_length_) {
         pt_w = (pt_w - camera_pos) / length * mp_->max_ray_length_ + camera_pos;
-        if (pt_w[2] < 0.2) continue;
+        if (pt_w[2] < 0.1) continue;
         tmp_flag = 0;
       } else
         tmp_flag = 1;
